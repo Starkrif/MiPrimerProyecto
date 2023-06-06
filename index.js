fa11b47bd16fd9882734151c36433a59f20dd66e
Alejandro Canelas Revollo <alejandrocanelas052@gmail.com>
2023-06-06 12:09:08 -0400

    mi primer seguimiento

---
+++ index.js
@@ -0,0 +1,6 @@
+function CambiarImagen1(){
+    document.getElementById("img1").src="https://elcomercio.pe/resizer/cjT2D2S5yvhtbrbU2v9VephXbrA=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/CC36FY2I25CVVORXFNLNIXEYMI.png"
+}
+function CambiarImagen2(){
+    document.getElementById("img1").src="https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/b801d02200400dcb6fd018d1f0baaa24.jpe"
+}
---
+++ variable.js
@@ -0,0 +1,6 @@
+// var vs let vs const
+var nombre="Alejandro"
+console.log(nombre);
+let edad=60;
+console.log(edad);
+const genero="M";
